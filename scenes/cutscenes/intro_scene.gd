2c19c68151bc60a7b15626df4d9b5eaf90073936
chpassos <crlshnrqpassos@gmail.com>
2024-11-20 17:03:39 -0300

    Finishing intro cutscene

--- scenes/cutscenes/intro_scene.gd
+++ scenes/cutscenes/intro_scene.gd
@@ -1,13 +1,20 @@
 extends Control
 
-var animation_list: Array = ["slide1_fadein", "slide2_fadein",  "slide3_fadein"]
+var animation_list: Array = [
+	"slide1_fadein",
+	"slide2_fadein",  
+	"slide3_fadein",
+	"slide4_fadein"
+	]
+
 var current_animation = 0
 var play_cur_anim = animation_list[0]
 
 var text_list: Array = [
-	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-	"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-	"ccccccccccccccccccccccccccccccccccccccccccccc"
+	"On an ordinary Sunday evening, it's natural for every parent to long for a bit of tranquility - a short but precious moment to sit back, breathe deeply, and enjoy a rare sense of peace amidst the usual busyness of family life.",
+	"However, as is often the case, children always seem to find a way to disrupt that fleeting moment of calm for their parents—whether it’s by creating a massive mess, accidentally breaking things, or getting themselves into some kind of mischief.",
+	"This mother had enough. While her love for children was beyond question, it was time for a valuable lesson. Determined and unstoppable, she vowed to set things straight by KICKING THE BUTT OF EVERY CHILDREN ON THE PLANET.",
+	"This is Mama Bazooka."
 	]
 	
 	
