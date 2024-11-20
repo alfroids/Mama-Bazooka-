af06fc52a8011b039480fa6f848e6542d9808f18
chpassos <crlshnrqpassos@gmail.com>
2024-11-19 23:23:32 -0300

    Enemy mom changes animation via code

--- scenes/mother/mother.gd
+++ scenes/mother/mother.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 @onready var player: CharacterBody2D = get_tree().get_first_node_in_group(&"player") as CharacterBody2D
 @onready var is_stunned: bool = false
 @onready var stun_timer: Timer = $StunTimer as Timer
-
+@onready var animated_sprite: AnimatedSprite2D = $MotherSprite as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if is_stunned:
@@ -17,6 +17,11 @@ func _physics_process(delta: float) -> void:
 	if player:
 		var player_dir: Vector2 = global_position.direction_to(player.global_position)
 		velocity = velocity.move_toward(speed * player_dir, delta * acceleration)
+		
+		if player_dir.y > 0:
+			animated_sprite.animation = "walk_front"
+		if player_dir.y < 0:
+			animated_sprite.animation = "walk_back"
 
 	move_and_slide()
 
