d099f37a28c21aff1690588c241b5312018f35d1
chpassos <crlshnrqpassos@gmail.com>
2024-11-19 23:18:46 -0300

    Added player animation via code

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var acceleration: float = 1600.0
 
 @onready var weapon_holder: Node2D = $WeaponHolder as Node2D
-
+@onready var animated_sprite: AnimatedSprite2D = $PlayerSprite as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	var input: Vector2 = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
@@ -15,6 +15,17 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(speed * input, 2 * delta * acceleration)
 	else:
 		velocity = velocity.move_toward(speed * input, delta * acceleration)
+	
+	if input == Vector2.DOWN:
+		animated_sprite.animation = "walk_front"
+	if input == Vector2.LEFT:
+		animated_sprite.animation = "walk_left"
+	if input == Vector2.RIGHT:
+		animated_sprite.animation = "walk_right"
+	if input == Vector2.UP:
+		animated_sprite.animation = "walk_back"
+	if input == Vector2.ZERO:
+		animated_sprite.animation = "idle_front"
 
 	move_and_slide()
 
