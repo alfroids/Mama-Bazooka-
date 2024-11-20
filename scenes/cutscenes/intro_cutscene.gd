8fc262cd8ab2e2a7153734b2ddb1f50305ae8808
chpassos <crlshnrqpassos@gmail.com>
2024-11-20 13:09:11 -0300

    Cutscenes changes via button, goes through the game

--- scenes/cutscenes/intro_cutscene.gd
+++ scenes/cutscenes/intro_cutscene.gd
@@ -6,7 +6,11 @@ var play_cur_anim = animation_list[0]
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer as AnimationPlayer
 
+
 func _on_button_pressed():
 	current_animation += 1
-	play_cur_anim = animation_list[current_animation]
-	animation_player.play(play_cur_anim)
+	if current_animation < len(animation_list):
+		play_cur_anim = animation_list[current_animation]
+		animation_player.play(play_cur_anim)
+	else:
+		get_tree().change_scene_to_file("res://scenes/main/main.tscn")
