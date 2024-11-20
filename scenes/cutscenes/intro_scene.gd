8ec4c95a0e9ce703a2723e739c19c0dce3df7197
chpassos <crlshnrqpassos@gmail.com>
2024-11-20 16:07:08 -0300

    Intro cutscenes now implemented with Control Nodes

--- scenes/cutscenes/intro_scene.gd
+++ scenes/cutscenes/intro_scene.gd
@@ -1,11 +1,10 @@
-extends Node2D
+extends Control
 
 var animation_list: Array = ["slide1_fadein", "slide2_fadein",  "slide3_fadein"]
 var current_animation = 0
 var play_cur_anim = animation_list[0]
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer as AnimationPlayer
-
 
 func _on_button_pressed():
 	current_animation += 1
