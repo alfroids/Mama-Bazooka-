8139b24205daca4d2bb30598171bd85a90c702d8
Alfredo Reis <alf.reismarin@gmail.com>
2024-11-22 23:10:07 -0300

    Finish tutorial and adjust door positions

--- scenes/tutorial/tutorial.gd
+++ scenes/tutorial/tutorial.gd
@@ -3,6 +3,11 @@ extends Node2D
 
 func _ready() -> void:
 	AudioController.play_main_song()
+	SignalBus.child_fainted.connect(_on_child_fainted)
+
+
+func _on_child_fainted() -> void:
+	$Tutorial3.visible = true
 
 
 func _on_easter_egg_timer_timeout() -> void:
